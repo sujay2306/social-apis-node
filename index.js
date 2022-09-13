8d1b58894734442c9624232557f1aa1735409644
sujay2306 <sujayks007@gmail.com>
2022-09-13 20:27:55 +0530

    social apis

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const format = require("date-format")
+const { restart } = require('nodemon')
+const app = express()
+const port = 3000
+
+app.get('/',(req,res) => {
+    res.status(200).send("<h1> Hello from backend </h1>");
+})
+
+app.get('/api/v1/instagram', (req,res) => {
+    const instaSocial = {
+        uername: "sujay_ks",
+        followers: 230,
+        following: 700,
+        date: format.asString("dd[MM] - hh:mm:ss",new Date()),
+    }
+    res.status(200).json(instaSocial)
+})
+
+app.get('/api/v1/facebook', (req,res) => {
+    const instaSocial = {
+        uername: "sujay_ks",
+        followers: 230,
+        following: 700,
+        date: format.asString("dd[MM] - hh:mm:ss",new Date()),
+    }
+    res.status(200).json(instaSocial)
+})
+
+app.get('/api/v1/linkedin', (req,res) => {
+    const instaSocial = {
+        uername: "sujay_ks",
+        followers: 20,
+        following: 70,
+        date: format.asString("dd[MM] - hh:mm:ss",new Date()),
+    }
+    res.status(200).json(instaSocial)
+})
+
+app.get("/api/v1/:id", (req,res) => {
+    console.log(req.params.id)
+    res.status(200).json({param:req.params.id})
+})
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
